63ba15ebaa103c333a131b85c8528f9c3444ebfa
dvirneuman <dvir477@gmail.com>
2020-03-25 14:26:31 -0700

    Loading json for non-default values work. semanthic changes at dumping

--- properties.py
+++ properties.py
@@ -1,18 +1,29 @@
 import json
+import jsonschema
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst, GLib
 
-def load_from_json(pipeline, json_file='props.json'):
+
+def load_from_json(pipeline, json_file='props.json', schema_file='props_schema.json'):
     with open(json_file, 'r') as f:
         properties = json.load(f)
-    print(properties)
+    with open(schema_file, 'r') as f:
+        schema = json.load(f)
+
+    try:
+        jsonschema.validate(properties, schema)
+    except jsonschema.exceptions.ValidationError as e:
+        print('not matching to schema', e)
+        return
+
     for element in pipeline.children:
         for prop in element.props:
             try:
                 json_config = properties[element.name][prop.name]
             except KeyError:
                 continue
-            if json_config[0] & 2:
-                setattr(element.props, prop.name, json_config[2])
-
+            setattr(element.props, prop.name, json_config)
 
 def dump_to_json(pipeline, json_file='props.json'):
     elements = {}
@@ -31,19 +42,19 @@ def dump_to_json(pipeline, json_file='props.json'):
 def gprop_to_json(gprop, default_value):
     gprop_type = gprop.value_type.name
     basic_prop = {'default' : default_value}
-    if 'int' in gprop_type:
-        basic_prop['type'] = 'number'
-        basic_prop['maximum'] = gprop.maximum
-        basic_prop['minimum'] = gprop.minimum
-    elif 'gboolean' == gprop_type:
+    if 'gboolean' == gprop_type:
         basic_prop['type'] = 'boolean'
-    elif 'char' in gprop_type:
-        basic_prop['type'] = 'string'
     elif hasattr(gprop, 'enum_class'):
         enum_options = gprop.enum_class.__enum_values__.values()
         basic_prop['enum'] = [x.value_nick for x in enum_options]
         basic_prop['type'] = 'string'
         basic_prop['default'] = default_value.value_nick
+    elif 'gchararray' == gprop_type:
+        basic_prop['type'] = 'string'
+    elif 'int' in gprop_type:
+        basic_prop['type'] = 'number'
+        basic_prop['maximum'] = gprop.maximum
+        basic_prop['minimum'] = gprop.minimum
     else:
         print('cant analyze type: ', gprop_type, gprop.name)
         return None
@@ -56,13 +67,12 @@ def dump_to_json_schema(pipeline, json_file='props_schema.json'):
         for prop in element.props:
             if prop.name == 'name':
                 continue
-            if prop.flags & 2:
-                json_serialized = gprop_to_json(prop, getattr(element.props, prop.name))
-                if json_serialized:
-                    current_element['properties'][prop.name] = json_serialized
+            if prop.flags & GObject.ParamFlags.WRITABLE:
+                json_serialized_prop = gprop_to_json(prop, getattr(element.props, prop.name))
+                if json_serialized_prop:
+                    current_element['properties'][prop.name] = json_serialized_prop
         elements[element.name] = current_element
 
-    print(elements)
     schema = {'type' : 'object', 'properties' : elements}
     with open(json_file, 'w') as f:
         json.dump(schema, f, indent=4)
