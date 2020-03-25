73356680ffa385b002cf680456348b3c3e7e6979
dvirneuman <dvir477@gmail.com>
2020-03-25 14:48:13 -0700

    Script that dump pipelien properties in json schema and can load properties from json

---
+++ properties.py
@@ -0,0 +1,79 @@
+import json
+import jsonschema
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst, GLib
+
+
+def load_from_json(pipeline, json_file='props.json', schema_file='props_schema.json'):
+    with open(json_file, 'r') as f:
+        properties = json.load(f)
+    with open(schema_file, 'r') as f:
+        schema = json.load(f)
+
+    try:
+        jsonschema.validate(properties, schema)
+    except jsonschema.exceptions.ValidationError as e:
+        print('not matching to schema', e)
+        return
+
+    for element in pipeline.children:
+        for prop in element.props:
+            try:
+                json_config = properties[element.name][prop.name]
+            except KeyError:
+                continue
+            setattr(element.props, prop.name, json_config)
+
+def dump_to_json(pipeline, json_file='props.json'):
+    elements = {}
+    for element in reversed(pipeline.children):
+        current_element = {}
+        for prop in element.props:
+            if 'Gst' in prop.value_type.name:
+                continue
+            current_element[prop.name] =  [prop.flags, 
+                                    prop.value_type.name, getattr(element.props, prop.name)]
+        elements[element.name] = current_element
+    print(elements)
+    with open(json_file, 'w') as f:
+        json.dump(elements, f, indent=4)
+
+def gprop_to_json(gprop, default_value):
+    gprop_type = gprop.value_type.name
+    basic_prop = {'default' : default_value}
+    if 'gboolean' == gprop_type:
+        basic_prop['type'] = 'boolean'
+    elif hasattr(gprop, 'enum_class'):
+        enum_options = gprop.enum_class.__enum_values__.values()
+        basic_prop['enum'] = [x.value_nick for x in enum_options]
+        basic_prop['type'] = 'string'
+        basic_prop['default'] = default_value.value_nick
+    elif 'gchararray' == gprop_type:
+        basic_prop['type'] = 'string'
+    elif 'int' in gprop_type:
+        basic_prop['type'] = 'number'
+        basic_prop['maximum'] = gprop.maximum
+        basic_prop['minimum'] = gprop.minimum
+    else:
+        print('cant analyze type: ', gprop_type, gprop.name)
+        return None
+    return basic_prop
+
+def dump_to_json_schema(pipeline, json_file='props_schema.json'):
+    elements = {}
+    for element in reversed(pipeline.children):
+        current_element = {'type' : 'object', 'properties' : {}}
+        for prop in element.props:
+            if prop.name == 'name':
+                continue
+            if prop.flags & GObject.ParamFlags.WRITABLE:
+                json_serialized_prop = gprop_to_json(prop, getattr(element.props, prop.name))
+                if json_serialized_prop:
+                    current_element['properties'][prop.name] = json_serialized_prop
+        elements[element.name] = current_element
+
+    schema = {'type' : 'object', 'properties' : elements}
+    with open(json_file, 'w') as f:
+        json.dump(schema, f, indent=4)
+
---
+++ test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst, GLib
+
+import properties
+
+def bus_call(bus, message, loop):
+    t = message.type
+    if t == Gst.MessageType.EOS:
+        sys.stdout.write("End-of-stream\n")
+        loop.quit()
+    elif t == Gst.MessageType.ERROR:
+        err, debug = message.parse_error()
+        sys.stderr.write("Error: %s: %s\n" % (err, debug))
+        loop.quit()
+    print('message type is {}'.format(t))
+    return True
+
+Gst.init(None)
+pipeline = Gst.parse_launch('videotestsrc ! autovideosink')
+
+loop = GLib.MainLoop()
+bus = pipeline.get_bus()
+bus.add_signal_watch()
+bus.connect ("message", bus_call, loop)
+
+element = pipeline.children[1]
+element.props.num_buffers = 20
+properties.dump_to_json_schema(pipeline)
+properties.load_from_json(pipeline)
+pipeline.set_state(Gst.State.PLAYING)
+
+loop.run()
+#When the process finishes, the pipeline stops
+import time
+time.sleep(1)
